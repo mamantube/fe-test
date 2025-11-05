ab705e3125fdabea2c6b0429d76ba7c291938f8d
mamantube <firmanhiday.id007@gmail.com>
2025-11-05 12:27:40 +0700

    update code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,10 @@
-import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {ConfigProvider} from "antd";
-import "antd/dist/reset.css";
+import { ReactNode } from "react";
 
-export const metadata: Metadata = {
-  title: "Product Dashboard",
-  description: "Technical Test",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <ConfigProvider>
-          {children}
-        </ConfigProvider>
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
